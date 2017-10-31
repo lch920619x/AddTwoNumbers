a78a8458bb2da6acdaa0065a884f197e51b59c36
lch <lch@172.23.24.155>
2017-10-31 16:28:46 +0800

    Allow user input

--- AddTwoNumbers/src/com/lch920619x/AddTwoNumbers/AddTwoNumbers.java
+++ AddTwoNumbers/src/com/lch920619x/AddTwoNumbers/AddTwoNumbers.java
@@ -2,6 +2,7 @@ package com.lch920619x.AddTwoNumbers;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class AddTwoNumbers {
 
@@ -9,7 +10,7 @@ public class AddTwoNumbers {
 		// TODO Auto-generated method stub
 		List<Integer> l1=new LinkedList<>();
 		List<Integer> l2=new LinkedList<>();
-		
+		Scanner sc = new Scanner(System.in);
 /*		l1.add(5);	
 		l1.add(4);	
 		l1.add(6);	
@@ -18,10 +19,28 @@ public class AddTwoNumbers {
 		l2.add(9);	
 	*/	
 
+		System.out.println("Now key in number 1:");
+		int num1 = sc.nextInt();
+		while (num1%10>0)
+		{
+			l1.add(num1%10);
+			num1=num1/10;
+		}
+		
+		System.out.println("Now key in number 2:");
+		int num2 = sc.nextInt();
+		while (num2%10>0)
+		{
+			l2.add(num2%10);
+			num2=num2/10;
+		}
+		
+		
 		Add add = new Add();
 		List<Integer> result = add.add(l1, l2);
 		
 		System.out.println(result);
+		sc.close();
 		
 	}
 
